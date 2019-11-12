b1ff7ad4e22074d3daf9c26327b636f7f744bce8
rahul <rahulpaliwal177@gmail.com>
2019-11-12 12:25:20 +0530

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CartComponent } from './cart/cart.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+ 
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: 'products',component:ProductDetailsComponent},
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart',component:CartComponent },
+    
+
+    ])
+
+    ],
+    
   providers: [],
   bootstrap: [AppComponent]
 })
